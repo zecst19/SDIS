6cfccb2869eee531cd12923340612341bfaef8b8
Gustavo Faria <gustavoduartefaria@gmail.com>
2017-03-29 10:14:32 +0100

    fixed errors

--- src/app/ClientApplication.java
+++ src/app/ClientApplication.java
@@ -1,42 +1,44 @@
+package app;
+
 import java.io.IOException;
 
 public class ClientApplication {
   public static void main(String[] args) throws IOException{
 
     if(args.length < 4 || args.length > 6){
-      System.out.println("Usage: java TestApp <peer_ap> <sub_protocol> <opnd_1> <opnd_2>")
+      System.out.println("Usage: java TestApp <peer_ap> <sub_protocol> <opnd_1> <opnd_2>");
       System.exit(1);
     }
 
-    private String sub_protocol = args[3];
+    String sub_protocol = args[3];
 
-    if(sub_protocol == 'BACKUP'){
+    if(sub_protocol == "BACKUP"){
       if(args.length != 6){
-        System.out.println("Wrong number of arguments for BACKUP protocol.")
+        System.out.println("Wrong number of arguments for BACKUP protocol.");
         System.exit(1);
       }
 
-    }else if(sub_protocol == 'RESTORE'){
+    }else if(sub_protocol == "RESTORE"){
       if(args.length != 5){
-        System.out.println("Wrong number of arguments for RESTORE protocol.")
+        System.out.println("Wrong number of arguments for RESTORE protocol.");
         System.exit(1);
       }
 
-    }else if(sub_protocol == 'DELETE'){
+    }else if(sub_protocol == "DELETE"){
       if(args.length != 5){
-        System.out.println("Wrong number of arguments for DELETE protocol.")
+        System.out.println("Wrong number of arguments for DELETE protocol.");
         System.exit(1);
       }
 
-    }else if(sub_protocol == 'RECLAIM'){
+    }else if(sub_protocol == "RECLAIM"){
       if(args.length != 5){
-        System.out.println("Wrong number of arguments for RECLAIM protocol.")
+        System.out.println("Wrong number of arguments for RECLAIM protocol.");
         System.exit(1);
       }
 
-    }else if(sub_protocol == 'STATE'){
+    }else if(sub_protocol == "STATE"){
       if(args.length != 4){
-        System.out.println("Wrong number of arguments for STATE protocol.")
+        System.out.println("Wrong number of arguments for STATE protocol.");
         System.exit(1);
       }
 
--- src/app/Interface.java
+++ src/app/Interface.java
@@ -1,3 +1,7 @@
+package app;
+
 public class Interface{
-  public static void main(String[] args) 
+    public static void main(String[] args){
+      
+    }
 }
