4d560d852c2b1cc3cc75f0b7d112e2ffda1c3a0f
zecst19 <zecst19@gmail.com>
2017-03-29 15:48:01 +0100

    peer update

--- src/app/Interface.java
+++ src/app/Interface.java
@@ -1,7 +1,36 @@
 package app;
 
 public class Interface{
-    public static void main(String[] args){
-      
+ 
+   /* public String backup(String file, int rep_degree){
+    	//fazer o backup, se funcionar retornar uma msg a dizer que o ficheiro file foi backed up com sucesso, se nao der msg a dizer que nao deu 
     }
+
+    public String backupenh(String file, int rep_degree){
+		//o mesmo que o primeiro
+    }
+
+    public String restore(String file){
+		//o mesmo que o primeiro
+    }
+
+    public String restoreenh(String file){
+		//o mesmo que o primeiro
+    }
+
+    public String delete(String file){
+		//o mesmo que o primeiro
+    }	
+
+    public String deleteenh(String file){
+		//o mesmo que o primeiro
+    }
+
+    public String reclaim(long space){
+		//o mesmo que o primeiro
+    }
+
+    public String reclaimenh(long space){
+		//o mesmo que o primeiro
+    }*/
 }
